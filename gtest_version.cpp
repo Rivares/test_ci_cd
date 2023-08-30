902b6b70f9636ac82da6535fb3d376062bf08f1c
Rivares <ura1994nikolaevic@gmail.com>
2023-08-31 01:32:16 +0300

    Attempt build proj with gtest

---
+++ gtest_version.cpp
@@ -0,0 +1,12 @@
+#define GOOGLE_TEST_MODULE gtest_version
+
+#include "lib.h"
+
+#include <gtest/gtest.h>
+
+
+
+TEST(TestGroupName, Subtest_1)
+{
+    ASSERT_TRUE(version() != 100);
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "lib.h"
-
+#include <gtest/gtest.h>
 #include <iostream>
 
-int main (int, char **) {
+
+int main ([[maybe_unused]]int argc, [[maybe_unused]]char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+
     std::cout << "build" << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+    return RUN_ALL_TESTS();
 }
