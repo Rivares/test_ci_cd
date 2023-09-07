b19e5ecf0ca1d45ead0066d6e74c34e92534c362
Rivares <rivares@protonmail.com>
2023-09-07 11:25:16 +0300

    Delete src/plugin_auto_lambda/CMakeFiles directory

--- src/plugin_auto_lambda/CMakeFiles/plugin_auto_lambda_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/plugin_auto_lambda_autogen"
-  "plugin_auto_lambda_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/plugin_auto_lambda_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
